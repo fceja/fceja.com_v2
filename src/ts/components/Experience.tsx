cca20fea93b2ab273b661060767d33b2f2f33094
fceja <88167774+fceja@users.noreply.github.com>
2024-05-22 16:46:26 -0700

    wip, add descript for exp card comp

--- src/ts/components/Experience.tsx
+++ src/ts/components/Experience.tsx
@@ -7,11 +7,20 @@ const Experience = () => {
             <div>
                 <ol>
                     <li className="mt-3">
-                        <div className="exp-card gap-3 sm:block md:flex lg:flex">
+                        <div className="exp-card p-2 gap-3 sm:block md:flex lg:flex">
                             <span className="card-year">2024 - PRESENT</span>
-                            <div className="card-desc">
-                                <div className="card-title">FrontEnd Engineer</div>
-                                <div className="card-paragraph mt-3">Build and maintain critical components used to construct Klaviyo’s frontend, across the whole product. Work closely with cross-functional teams, including developers, designers, and product managers, to implement and advocate for best practices in web accessibility.</div>
+                            <div>
+
+                                <div className="card-desc">
+                                    <div className="card-title">Frontend Engineer</div>
+                                    <div className="card-paragraph mt-3">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets.</div>
+                                </div>
+                                <div className="tags block mt-3">
+                                    <span className="py-1 px-2">JavaScript</span>
+                                    <span className="py-1 px-2">TypeScript</span>
+                                    <span className="py-1 px-2">React</span>
+                                    <span className="py-1 px-2">Node.js</span>
+                                </div>
                             </div>
                         </div>
                     </li>
