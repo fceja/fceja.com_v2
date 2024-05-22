500fc82cd9ed68ce67fe657f2659e339fa46f0ee
fceja <88167774+fceja@users.noreply.github.com>
2024-05-22 12:00:31 -0700

    add header components with scss

---
+++ src/ts/components/Header.tsx
@@ -0,0 +1,12 @@
+import "@scss/components/Header.scss"
+
+const Header = () => {
+    return (
+        <header>
+            <h1>Francisco Ceja</h1>
+            <h3>QA / Automater / Python Developer</h3>
+            <p>I like to code apps and stuff.</p>
+        </header>
+    )
+}
+export default Header
