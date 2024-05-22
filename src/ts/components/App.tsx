e95c9fd7037130e12f78eca1ecf4dfc17d895b3b
fceja <88167774+fceja@users.noreply.github.com>
2024-05-22 10:24:58 -0700

    refactor folder structure, add scss and bootstrap

---
+++ src/ts/components/App.tsx
@@ -0,0 +1,12 @@
+import "@scss/components/App.scss"
+import Spotlight from "@components/Spotlight";
+
+function App() {
+  return (
+    <div id="app">
+      <Spotlight />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/ts/components/Spotlight.tsx
@@ -0,0 +1,45 @@
+import React, { useState, useEffect } from 'react';
+
+import '@scss/components/Spotlight.scss';
+
+interface Position {
+    x: number;
+    y: number;
+}
+
+const Spotlight: React.FC = () => {
+    const [position, setPosition] = useState<Position>({ x: 0, y: 0 });
+    const [visible, setVisible] = useState<boolean>(false);
+
+    const handleMouseMove = (e: MouseEvent) => {
+        setPosition({ x: e.clientX, y: e.clientY });
+        setVisible(true);
+    };
+
+    const handleMouseLeave = () => {
+        setVisible(false);
+    };
+
+    useEffect(() => {
+        window.addEventListener('mousemove', handleMouseMove);
+        window.addEventListener('mouseout', handleMouseLeave);
+
+        return () => {
+            window.removeEventListener('mousemove', handleMouseMove);
+            window.removeEventListener('mouseout', handleMouseLeave);
+        };
+    }, []);
+
+    return (
+        <div
+            className="spotlight"
+            style={{
+                left: `${position.x}px`,
+                top: `${position.y}px`,
+                opacity: visible ? 1 : 0,
+            }}
+        ></div>
+    );
+};
+
+export default Spotlight;
