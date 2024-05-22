a7632eb159a010514607d526366e81f1a3e470a5
fceja <88167774+fceja@users.noreply.github.com>
2024-05-22 12:03:12 -0700

    refactor to use root and index comp

--- src/ts/Index.tsx
+++ src/ts/Index.tsx
@@ -1,11 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
-import "@scss/Root.scss"
-import App from './components/App.tsx'
+import Root from '@components/Root.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <Root />
   </React.StrictMode>,
 )
--- src/ts/components/App.tsx
+++ src/ts/components/App.tsx
@@ -1,10 +1,14 @@
-import "@scss/components/App.scss"
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import "@scss/Global.scss"
+import Header from "@components/Header";
 import Spotlight from "@components/Spotlight";
 
 function App() {
   return (
     <div id="app">
       <Spotlight />
+      <Header />
     </div>
   );
 }
