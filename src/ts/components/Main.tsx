136b9c452161b2faa5b04d379afc6c72c37c5b7a
fceja <88167774+fceja@users.noreply.github.com>
2024-05-22 21:21:06 -0700

    wip - experice css updates

--- src/ts/components/Main.tsx
+++ src/ts/components/Main.tsx
@@ -3,7 +3,12 @@ import Experience from "@components/Experience"
 
 const Main = () => {
     return (
-        <main className="sm:mt-3 md:mt-3 lg:w-50 lg:mlr-auto">
+        <main
+            className="sm:mt-3 md:mt-3 lg:w-50 lg:mlr-auto"
+            style={{
+                width: "100%",
+                maxWidth: "1000px"
+            }}>
             <About />
             <Experience />
         </main>
--- src/ts/pages/HomePage.tsx
+++ src/ts/pages/HomePage.tsx
@@ -3,7 +3,12 @@ import Main from "@components/Main";
 
 const HomePage = () => {
     return (
-        <div className="homepage p-4 lg:gap-2 lg:flex">
+        <div
+            className="homepage p-4 lg:gap-2 lg:flex"
+            style={{
+                gap: "5em"
+            }}
+        >
             <Header />
             <Main />
         </div >
