f5003fc5cfafbac594cae42254a041ebace9f360
fceja <88167774+fceja@users.noreply.github.com>
2024-05-23 13:04:40 -0700

    implement project links component

--- src/ts/components/Projects.tsx
+++ src/ts/components/Projects.tsx
@@ -1,12 +1,16 @@
 import "@scss/components/Projects.scss"
 import Tags from "@common/components/Tags"
+import ProjectLinks from "@components/ProjectLinks"
+
 const projectData = [
     {
         title: 'Profile App',
         paragraph: "A web app that can be used for a user/company profile.",
         tags: [
             "TypeScript", "React JS", "Redux JS", "Bootstrap", "AWS:S3"
-        ]
+        ],
+        gitHubLink: "https://github.com/fceja/profile_app",
+        urlLink: "http://dev.client.profile-app.s3-website-us-west-1.amazonaws.com/"
     }
 ]
 
@@ -18,33 +22,35 @@ const Projects = () => {
                     return (
                         <div key={`proj-${i}`}>
                             <h2>Projects</h2>
-                            <div className={`project-${i} d-flex gap-3 mt-3`}>
+                            <div className={`project-${i} gap-3 mt-3 md:flex lg:flex`}>
                                 <div
                                     className="proj-img"
                                     style={{
-                                        minWidth: "125px",
-                                        height: "100px",
                                         backgroundColor: "lightgray",
+                                        height: "100px",
+                                        minWidth: "125px",
+                                        maxWidth: "125px",
                                     }}
-                                >[Image container]</div>
-                                <div className="proj-desc w-100">
-                                    <span className="proj-title">{elem.title}</span>
-                                    <span
-                                        className="proj-arrow ml-1"
-                                        style={{ margin: "0 0 0 10px" }}
-                                    >
-                                        [Arrow]
-                                    </span>
+                                >
+                                    [Image container]
+                                </div>
+                                {<div className="proj-details w-100 sm:mt-10px">
+                                    <div className="proj-header d-flex">
+                                        <div className="proj-title ">
+                                            {elem.title}
+                                        </div>
+                                        <ProjectLinks gitHubLink={elem.gitHubLink} urlLink={elem.urlLink} />
+                                    </div>
                                     <p className="proj-p mt-4">{elem.paragraph}</p>
                                     <Tags tagData={elem.tags} parentIndex={i} />
-                                </div>
+                                </div>}
 
                             </div>
                         </div>
                     )
                 })}
             </>
-        </section>
+        </section >
     )
 }
 export default Projects
