91692454c2f9c8fdc6e94da82a99fb9ac13bda4c
fceja <88167774+fceja@users.noreply.github.com>
2024-06-01 13:34:36 -0700

    minor changes

--- src/ts/components/home/semantic/AboutSection.tsx
+++ src/ts/components/home/semantic/AboutSection.tsx
@@ -5,7 +5,7 @@ const AboutSection = () => {
     return (
         <section className="about">
             <h2 className="m-0">ABOUT</h2>
-            <p className="m-0 mt-3 p-0">{aboutContent}</p>
+            <p className="m-0 mt-4 p-0">{aboutContent}</p>
         </section>
     )
 }
