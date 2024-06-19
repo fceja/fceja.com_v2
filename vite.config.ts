97eb014d7d2d64fcea7081ae4fe000a7c93f57b3
fceja <88167774+fceja@users.noreply.github.com>
2024-06-19 14:29:47 -0700

    upgrade to use react 19 w/ compiler

--- vite.config.ts
+++ vite.config.ts
@@ -2,20 +2,33 @@ import path from "path";
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+//https://react.dev/learn/react-compiler#what-does-the-compiler-do
+const ReactCompilerConfig = {}
+
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@assets": path.resolve(__dirname, "./src/assets"),
-      "@common": path.resolve(__dirname, "./src/ts/common"),
-      "@components": path.resolve(__dirname, "./src/ts/components"),
-      "@content": path.resolve(__dirname, "./src/ts/content"),
-      "@hooks": path.resolve(__dirname, "./src/ts/hooks"),
-      "@pages": path.resolve(__dirname, "./src/ts/pages"),
-      "@scss": path.resolve(__dirname, "./src/scss"),
+export default defineConfig(() => {
+  return {
+    plugins: [
+      react({
+        babel: {
+          plugins: [
+            ["babel-plugin-react-compiler", ReactCompilerConfig],
+          ],
+        },
+      }),
+    ],
+    resolve: {
+      alias: {
+        "@assets": path.resolve(__dirname, "./src/assets"),
+        "@common": path.resolve(__dirname, "./src/ts/common"),
+        "@components": path.resolve(__dirname, "./src/ts/components"),
+        "@content": path.resolve(__dirname, "./src/ts/content"),
+        "@hooks": path.resolve(__dirname, "./src/ts/hooks"),
+        "@pages": path.resolve(__dirname, "./src/ts/pages"),
+        "@scss": path.resolve(__dirname, "./src/scss"),
+      }
+    }, server: {
+      port: 3001,
     }
-  }, server: {
-    port: 3001, // Replace with your desired port number
-  },
+  };
 });
