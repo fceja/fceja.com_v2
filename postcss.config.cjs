81a4bba0f571eecb4fe0ae775093b2bdfc6c2971
fceja <88167774+fceja@users.noreply.github.com>
2024-07-15 23:59:09 -0700

    styles to target safari browser only

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,5 +1,15 @@
 module.exports = {
-    plugins: {
-      autoprefixer: {},
-    },
-  };
+  plugins: [
+    require('postcss-import'),
+    require('autoprefixer'),
+    function (css) {
+      /* Safari 7.1+ Specific Styles, after scss processing */
+      css.append(`
+        _::-webkit-full-page-media, _:future, :root .headlines-container {
+          background: rgba(85, 85, 85, 0.498);
+          backdrop-filter: none;
+        }
+      `);
+    }
+  ]
+}
