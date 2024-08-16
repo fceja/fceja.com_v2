0fba6ada7eb5c1ce112ac85ae9db4da46e3cc291
fceja <88167774+fceja@users.noreply.github.com>
2024-08-16 07:47:25 -0700

    add loading spinner

---
+++ src/ts/common/components/LoadingSpinner.tsx
@@ -0,0 +1,15 @@
+import "@scss/common/components/LoadingSpinner.scss";
+
+interface LoadingI {
+    className?: string
+}
+const LoadingSpinner = (props: LoadingI) => {
+    const { className } = props
+    return (
+        <div className={className ? `loading-${className}` : 'loading'}>
+            <div className="loading-spinner"></div>
+        </div>
+    )
+}
+
+export default LoadingSpinner
