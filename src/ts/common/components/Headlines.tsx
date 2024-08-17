6ec7cc4c0fda9eb529794260e6b1822cfdcfcd98
fceja <88167774+fceja@users.noreply.github.com>
2024-08-17 15:35:54 -0700

    safari animation bug fix

--- src/ts/common/components/Headlines.tsx
+++ src/ts/common/components/Headlines.tsx
@@ -45,7 +45,6 @@ const Headlines = () => {
     }
 
     fetchData()
-
   }, [])
 
   useEffect(() => {
@@ -61,12 +60,68 @@ const Headlines = () => {
     });
   }, [responseData]);
 
+  /* safari had issues with animations applied via css, we apply with javascript instead */
+  useEffect(() => {
+    if (!headlines) return;
+
+    const headlineList = document.querySelector('.headline-list') as HTMLElement;
+    if (!headlineList) return;
+
+    /* define pause animation */
+    const pauseAnimation = () => {
+      headlineList.style.animationPlayState = 'paused';
+      headlineList.classList.remove('animation-running');
+      headlineList.classList.add('animation-paused');
+    };
+
+    /* define start animation */
+    const startAnimation = () => {
+      headlineList.style.animationPlayState = 'running';
+      headlineList.classList.remove('animation-paused');
+      headlineList.classList.add('animation-running');
+    };
+
+    /* add animation styles and run */
+    headlineList.style.animationName = 'scrollLeft';
+    headlineList.style.animationDuration = '200s';
+    headlineList.style.animationTimingFunction = 'linear';
+    headlineList.style.animationIterationCount = 'infinite';
+    headlineList.style.animationPlayState = 'running';
+    headlineList.style.willChange = 'transform';
+    headlineList.classList.add('animation-running');
+
+    /* determine if touch or mouse device */
+    const isTouchDevice = 'ontouchstart' in window || navigator.maxTouchPoints > 0;
+    if (isTouchDevice) {
+      headlineList.addEventListener('touchstart', () => {
+        pauseAnimation();
+
+        setTimeout(() => {
+          startAnimation();
+        }, 2000);
+      });
+    } else {
+      headlineList.addEventListener('mouseenter', pauseAnimation);
+      headlineList.addEventListener('mouseleave', startAnimation);
+    }
+
+    /* cleanup event listeners */
+    return () => {
+      if (isTouchDevice) {
+        headlineList.removeEventListener('touchstart', pauseAnimation);
+        headlineList.removeEventListener('touchend', startAnimation);
+      } else {
+        headlineList.removeEventListener('mouseenter', pauseAnimation);
+        headlineList.removeEventListener('mouseleave', startAnimation);
+      }
+    };
+  }, [headlines]);
 
   return (
     <>
       {isLoading && <LoadingSquare className="headlines" />}
       {!isLoading && responseData && headlines &&
-        <div className={`headlines-container${!isPlatformSupportedRef.current ? "-safari" : ""}`}>
+        <div className="headlines-container">
           <ul className="headline-list d-flex align-items-center">
             {headlines.map((elem, index) => {
               return (
