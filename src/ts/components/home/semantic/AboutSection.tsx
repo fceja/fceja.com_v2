3b47f585f075ed091ad06035a86ce6d01be0c3ce
fceja <88167774+fceja@users.noreply.github.com>
2024-08-21 09:36:17 -0700

    add about content div

--- src/ts/components/home/semantic/AboutSection.tsx
+++ src/ts/components/home/semantic/AboutSection.tsx
@@ -10,20 +10,22 @@ const AboutSection = () => {
         <section className="about">
             <h2 className="">ABOUT</h2>
             <Collapsible>
-                <p className="mt-4">
-                    Hello, my name is <strong className="hover-text">Francisco</strong>. <br />
-                    I am a developer with a background in <strong className="hover-text">QA</strong>, <strong className="hover-text">Automation</strong>, and <strong className="hover-text">Software Development</strong>.
-                </p>
-                <p>
-                    I've had the opportunity to work for a <strong><a href="https://valuestreamengineering.com/" target="_blank" className="hover-text">start-up</a></strong>, where I've had the privilege to work with multiple clients and tech stacks
-                    — gaining invaluable knowledge and skills.
-                </p>
-                <p>
-                    When I'm away from the keyboard, I'm usually reading, running, hanging out
-                    with friends and family, or attending to my <span className="plant-icon">plants</span>.
-                </p>
+                <div className="about-content p-1">
+                    <p>
+                        Hello, my name is <strong className="hover-text">Francisco</strong>. <br />
+                        I am a developer with a background in <strong className="hover-text">QA</strong>, <strong className="hover-text">Automation</strong>, and <strong className="hover-text">Software Development</strong>.
+                    </p>
+                    <p>
+                        I've had the opportunity to work for a <strong><a href="https://valuestreamengineering.com/" target="_blank" className="hover-text">start-up</a></strong>, where I've had the privilege to work with multiple clients and tech stacks
+                        — gaining invaluable knowledge and skills.
+                    </p>
+                    <p>
+                        When I'm away from the keyboard, I'm usually reading, running, hanging out
+                        with friends and family, or attending to my <span className="plant-icon">plants</span>.
+                    </p>
+                </div>
             </Collapsible>
-        </section>
+        </section >
     )
 }
 export default AboutSection
