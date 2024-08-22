4eeb1d47cd0990216a574c4de9a1bd75120f9f99
fceja <88167774+fceja@users.noreply.github.com>
2024-08-21 19:05:23 -0700

    use javascript for animation instead of css

--- src/ts/common/components/Collapsible.tsx
+++ src/ts/common/components/Collapsible.tsx
@@ -10,31 +10,89 @@ const Collapsible: React.FC<CollapsibleI> = (props) => {
     const { children } = props
     const [isCollapsed, setIsCollapsed] = useState(true)
     const outerContainerRef = useRef<HTMLDivElement | null>(null);
+    const stationaryContainerRef = useRef<HTMLDivElement | null>(null);
+    const [firstMount, setFirstMount] = useState(true);
 
     useEffect(() => {
-        const div = outerContainerRef.current
-        if (!div) { return };
+        setFirstMount(false)
+    }, [])
 
-        if (isCollapsed) {
-            const checkOpacity = () => {
-                const opacity = window.getComputedStyle(div).opacity;
+    useEffect(() => {
+        const divOuter = outerContainerRef.current
+        const divStationary = stationaryContainerRef.current
+        if (!divOuter || !divStationary) { return };
 
-                if (Number(opacity) < 0.15) {
-                    div.style.position = "absolute";
-                    div.style.visibility = "hidden";
-                } else {
-                    requestAnimationFrame(checkOpacity);
-                }
-            }
+        /* handle expanded to collapsed, div's are '-collapsed' */
+        const handleCollapse = async () => {
+            divOuter.style.transition = 'border 1s';
+            divOuter.style.border = 'solid 1px rgba(255, 255, 255, 1)';
 
-            requestAnimationFrame(checkOpacity);
+            await new Promise<void>((resolve) => {
+                /* outer div */
+                divOuter.style.transition = "transform 1s";
+                divOuter.style.transform = "scale(0.1) rotate(-360deg)";
 
-        } else {
-            div.style.position = "static"
-            div.style.visibility = "visible"
+                /* inner stationary div */
+                divStationary.style.transition = "transform 1s";
+                divStationary.style.transform = "rotate(360deg)";
+
+                // wait for scale transition to complete, then hide and remove from doc flow
+                divOuter.addEventListener('transitionend', function handler(event) {
+                    if (event.propertyName === 'transform') {
+                        divOuter.removeEventListener('transitionend', handler);
+                        divOuter.style.position = "absolute";
+                        divOuter.style.visibility = "hidden";
+                        resolve();
+                    }
+                });
+            });
         }
 
+        /* handle collapsed to expanded, div's are '-expanded' */
+        const handleExpand = async () => {
+            /* outer div */
+            divOuter.style.position = "relative"
+            divOuter.style.visibility = "visible"
+            divOuter.style.transition = 'border 1s, transform 1s';
+            divOuter.style.border = 'solid 1px rgba(255, 255, 255, 1)';
+            divOuter.style.transform = 'scale(1) rotate(360deg)';
+
+            /* inner stationary div */
+            divStationary.style.transition = "transform 1s";
+            divStationary.style.transform = "rotate(-360deg)";
+
+            /* wait for transition to complete, then hide outer border */
+            await new Promise<void>((resolve) => {
+                divOuter.addEventListener('transitionend', function handler() {
+                    divOuter.removeEventListener('transitionend', handler);
+                    divOuter.style.transition = 'border-color 1s';
+                    divOuter.style.borderColor = 'rgba(255, 255, 255, 0)';
+                    console.log('resolved handle expand')
+                    resolve();
+                });
+            });
+        }
+
+        if (isCollapsed) {
+            if (firstMount) {
+                handleFirstMount()
+            } else {
+                handleCollapse()
+            }
+        }
+        else { handleExpand() }
+
     }, [isCollapsed])
+
+    const handleFirstMount = () => {
+        const divOuter = outerContainerRef.current
+        const divStationary = stationaryContainerRef.current
+        if (!divOuter || !divStationary) { return };
+
+        divOuter.style.position = "absolute"
+        divOuter.style.visibility = "hidden"
+        divOuter.style.transform = "scale(0.1)"
+    }
 
     const handlePlusOrMinusClick = () => { setIsCollapsed(!isCollapsed) }
 
@@ -45,8 +103,11 @@ const Collapsible: React.FC<CollapsibleI> = (props) => {
                 :
                 <div className="icon-minus" onClick={handlePlusOrMinusClick}>-</div>
             }
-            <div ref={outerContainerRef} className={`outer-container${isCollapsed ? "-collapsed" : "-expanded"}`}>
-                <div className={`stationary-content${isCollapsed ? "-collapsed" : "-expanded"}`}>
+            <div
+                ref={outerContainerRef}
+                className={`outer-container${isCollapsed ? "-collapsed" : "-expanded"}`}
+            >
+                <div ref={stationaryContainerRef} className={`stationary-content${isCollapsed ? "-collapsed" : "-expanded"}`}>
                     {children}
                 </div>
             </div>
