6444032f03e4b95a0e7525dea2286f68f2a898b0
fceja <88167774+fceja@users.noreply.github.com>
2024-08-22 09:30:57 -0700

    add condition to check if animation is playing

--- src/ts/common/components/Collapsible.tsx
+++ src/ts/common/components/Collapsible.tsx
@@ -1,104 +1,117 @@
 import { useEffect, useRef, useState } from "react";
-
-import "@scss/common/components/Collapsible.scss"
+import "@scss/common/components/Collapsible.scss";
 
 interface CollapsibleI {
     children: React.ReactNode;
 }
 
 const Collapsible: React.FC<CollapsibleI> = (props) => {
-    const { children } = props
-    const [isCollapsed, setIsCollapsed] = useState(true)
+    const { children } = props;
+    const [isCollapsed, setIsCollapsed] = useState(true);
+    const [isTransitioning, setIsTransitioning] = useState(false);
     const outerContainerRef = useRef<HTMLDivElement | null>(null);
     const stationaryContainerRef = useRef<HTMLDivElement | null>(null);
     const [firstMount, setFirstMount] = useState(true);
 
     useEffect(() => {
-        setFirstMount(false)
-    }, [])
+        setFirstMount(false);
+    }, []);
 
     useEffect(() => {
-        const divOuter = outerContainerRef.current
-        const divStationary = stationaryContainerRef.current
-        if (!divOuter || !divStationary) { return };
+        const divOuter = outerContainerRef.current;
+        const divStationary = stationaryContainerRef.current;
+        if (!divOuter || !divStationary) return;
 
-        /* handle expanded to collapsed, div's are '-collapsed' */
-        const handleCollapse = async () => {
-            divOuter.style.transition = 'border 1s';
-            divOuter.style.border = 'solid 1px rgba(255, 255, 255, 1)';
+        const handleTransition = async () => {
+            if (!isCollapsed) {
+                await handleExpand();
+                return
+            }
 
-            await new Promise<void>((resolve) => {
-                /* outer div */
-                divOuter.style.transition = "transform 1s";
-                divOuter.style.transform = "scale(0.1) rotate(-360deg)";
+            if (firstMount) { handleFirstMount(); }
+            else { await handleCollapse(); }
+        };
 
-                /* inner stationary div */
-                divStationary.style.transition = "transform 1s";
-                divStationary.style.transform = "rotate(360deg)";
+        handleTransition();
 
-                // wait for scale transition to complete, then hide and remove from doc flow
-                divOuter.addEventListener('transitionend', function handler(event) {
-                    if (event.propertyName === 'transform') {
-                        divOuter.removeEventListener('transitionend', handler);
-                        divOuter.style.position = "absolute";
-                        divOuter.style.visibility = "hidden";
-                        resolve();
-                    }
-                });
-            });
-        }
+    }, [isCollapsed]);
 
-        /* handle collapsed to expanded, div's are '-expanded' */
-        const handleExpand = async () => {
-            /* outer div */
-            divOuter.style.position = "relative"
-            divOuter.style.visibility = "visible"
-            divOuter.style.transition = 'border 1s, transform 1s';
-            divOuter.style.border = 'solid 1px rgba(255, 255, 255, 1)';
-            divOuter.style.transform = 'scale(1) rotate(360deg)';
 
-            /* inner stationary div */
+    const handleCollapse = async () => {
+        console.log('starting - handleCollapse');
+        setIsTransitioning(true)
+        const divOuter = outerContainerRef.current;
+        const divStationary = stationaryContainerRef.current;
+        if (!divOuter || !divStationary) return;
+
+        divOuter.style.transition = 'border 1s';
+        divOuter.style.border = 'solid 1px rgba(255, 255, 255, 1)';
+
+        await new Promise<void>((resolve) => {
+            divOuter.style.transition = "transform 1s";
+            divOuter.style.transform = "scale(0.05)";
+
             divStationary.style.transition = "transform 1s";
-            divStationary.style.transform = "rotate(-360deg)";
+            divStationary.style.transform = "rotate(0deg)";
 
-            /* wait for transition to complete, then hide outer border */
-            await new Promise<void>((resolve) => {
-                divOuter.addEventListener('transitionend', function handler() {
-                    divOuter.removeEventListener('transitionend', handler);
+            divOuter.addEventListener('transitionend', function collapseEventHandler(event) {
+                if (event.propertyName === 'transform') {
+                    divOuter.removeEventListener('transitionend', collapseEventHandler);
+                    divOuter.style.position = "absolute";
+                    divOuter.style.visibility = "hidden";
+                    console.log('resolved - handleCollapse');
+                    setIsTransitioning(false)
+                    resolve();
+                }
+            });
+        });
+    };
+
+    const handleExpand = async () => {
+        console.log('starting - handleExpand');
+        setIsTransitioning(true)
+        const divOuter = outerContainerRef.current;
+        const divStationary = stationaryContainerRef.current;
+        if (!divOuter || !divStationary) return;
+
+        divOuter.style.position = "relative";
+        divOuter.style.visibility = "visible";
+        divOuter.style.transition = 'border 1s, transform 1s';
+        divOuter.style.border = 'solid 1px rgba(255, 255, 255, 1)';
+        divOuter.style.transform = 'scale(1) rotate(180deg)';
+
+        divStationary.style.transition = "transform 1s";
+        divStationary.style.transform = "rotate(-180deg)";
+
+        await new Promise<void>((resolve) => {
+            divOuter.addEventListener('transitionend', function expandEventHandler(event) {
+                if (event.propertyName === 'transform') {
+                    divOuter.removeEventListener('transitionend', expandEventHandler);
                     divOuter.style.transition = 'border-color 1s';
                     divOuter.style.borderColor = 'rgba(255, 255, 255, 0)';
-                    console.log('resolved handle expand')
-                    resolve();
-                });
+                    console.log('resolved - handleExpand');
+                    resolve(setIsTransitioning(false));
+                }
             });
-        }
-
-        if (isCollapsed) {
-            if (firstMount) {
-                handleFirstMount()
-            } else {
-                handleCollapse()
-            }
-        }
-        else { handleExpand() }
-
-    }, [isCollapsed])
+        });
+    };
 
     const handleFirstMount = () => {
-        const divOuter = outerContainerRef.current
-        const divStationary = stationaryContainerRef.current
-        if (!divOuter || !divStationary) { return };
+        const divOuter = outerContainerRef.current;
+        const divStationary = stationaryContainerRef.current;
+        if (!divOuter || !divStationary) return;
 
-        /* outer div */
-        divOuter.style.position = "absolute"
-        divOuter.style.visibility = "hidden"
-        divOuter.style.transform = "scale(0.1)"
+        divOuter.style.position = "absolute";
+        divOuter.style.visibility = "hidden";
+        divOuter.style.transform = "scale(0.1)";
 
-        /* inner stationary div */
-        divStationary.style.display = "inline-block"
-    }
+        divStationary.style.display = "inline-block";
+    };
 
-    const handlePlusOrMinusClick = () => { setIsCollapsed(!isCollapsed) }
+    const handlePlusOrMinusClick = () => {
+        if (isTransitioning) { return }
+        setIsCollapsed(!isCollapsed);
+    };
 
     return (
         <>
@@ -116,6 +129,7 @@ const Collapsible: React.FC<CollapsibleI> = (props) => {
                 </div>
             </div>
         </>
-    )
-}
-export default Collapsible
+    );
+};
+
+export default Collapsible;
